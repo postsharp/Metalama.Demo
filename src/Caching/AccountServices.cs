b7b95e27776a016b72197a711a066aa292d70ee6
GaelFraiteur <gael@postsharp.net>
2024-01-30 18:23:35 +0100

    Adding caching.

---
+++ src/Caching/AccountServices.cs
@@ -0,0 +1,38 @@
+﻿
+
+  using Metalama.Patterns.Caching;
+  using Metalama.Patterns.Caching.Aspects;
+
+  [CachingConfiguration(ProfileName = "Account")] // See Program.cs for the configuration of the "Account" caching profile.
+  internal class AccountServices
+  {
+    [Cache]
+    public static Account GetAccount(int id)
+    {
+      Console.WriteLine($">> Retrieving the account {id} from database...");
+      Thread.Sleep(1000);
+
+      var account = new Account { AccountId = id };
+
+      CachingService.Default.AddDependency(account);
+
+      return account;
+    }
+
+    [Cache]
+    public static IEnumerable<Account> GetAccountsOfCustomer(int customerId)
+    {
+      // Dependencies of GetAccount are automatically added to GetAccountsOfCustomer.
+      yield return GetAccount(1);
+      yield return GetAccount(2);
+    }
+
+    public static void UpdateAccount(Account account)
+    {
+      Console.WriteLine($">> Updating the account {account.AccountId} in database...");
+      Thread.Sleep(1000);
+
+      // This will invalidate both GetAccount and GetAccountsOfCustomer.
+      CachingService.Default.Invalidate(account);
+    }
+  }
---
+++ src/Caching/Program.cs
@@ -0,0 +1,41 @@
+﻿using Metalama.Patterns.Caching;
+using Metalama.Patterns.Caching.Aspects;
+
+[assembly: CachingConfiguration( UseDependencyInjection = false )]
+
+[CachingConfiguration( AbsoluteExpiration = 5 )]
+internal class Program
+{
+    private static void Main( string[] args )
+    {
+        // Uncomment the next line for detailed logging.
+        // LoggingServices.DefaultBackend.DefaultVerbosity.SetMinimalLevel(LogLevel.Debug, LoggingRoles.Caching);
+
+        var accountProfile = new CachingProfile( "Account" ) { AbsoluteExpiration = TimeSpan.FromSeconds( 10 ) };
+        CachingService.Default = CachingService.Create( builder => builder.AddProfile( accountProfile ) );
+
+
+        // Testing direct invalidation.
+        Console.WriteLine( "Retrieving the customer for the 1st time should hit the database." );
+        CustomerServices.GetCustomer( 1 );
+        Console.WriteLine( "Retrieving the customer for the 2nd time should NOT hit the database." );
+        CustomerServices.GetCustomer( 1 );
+        Console.WriteLine( "This should invalidate the GetCustomer method." );
+        CustomerServices.UpdateCustomer( 1, "New name" );
+        Console.WriteLine( "This should hit the database again because GetCustomer has been invalidated." );
+        CustomerServices.GetCustomer( 1 );
+
+        // Testing indirect invalidation (dependencies).
+        Console.WriteLine( "Retrieving the account list for the 1st time should hit the database." );
+        _ = AccountServices.GetAccountsOfCustomer( 1 );
+        Console.WriteLine( "Retrieving the account list for the 2nt time should NOT hit the database." );
+        var accounts = AccountServices.GetAccountsOfCustomer( 1 );
+        Console.WriteLine( "This should invalidate the accounts" );
+        AccountServices.UpdateAccount( accounts.First() );
+        Console.WriteLine(
+            "This should hit the database again because GetAccountsOfCustomer has been invalidated." );
+        _ = AccountServices.GetAccountsOfCustomer( 1 );
+
+        Console.WriteLine( "Done!" );
+    }
+}
