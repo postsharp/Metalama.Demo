1c27512ed0811431bdda0486a80782cf6eee4b2c
GaelFraiteur <gael@postsharp.net>
2024-01-31 09:13:01 +0100

    Adding contracts.

---
+++ src/Contracts/Customer.cs
@@ -0,0 +1,14 @@
+﻿using Metalama.Patterns.Contracts;
+
+namespace Contracts;
+
+public class Customer
+{
+    public string Name { get; set; }
+    
+    [Email]
+    public string Email { get; set; }
+    
+    [CreditCard]
+    public string? CreditCardNumber { get; set; }
+}
