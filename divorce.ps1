9e97cbcdddce9ef72ed8362ee851bf83b6a74976
GaelFraiteur <gael@postsharp.net>
2024-04-12 16:04:35 +0200

    Improved divorce script.

--- divorce.ps1
+++ divorce.ps1
@@ -7,7 +7,26 @@
      throw 'Uncommitted changes detected. Please commit or stash your changes.'
  }
 
+ # Create a new branch
+ $currentTimestamp  = Get-Date -Format "yyyyMMdd-HHmmss"
+ $branchName = "divorce-$currentTimestamp"
+ git checkout -b $branchName
+
+ # Format code
+ dotnet format
+
+ # Commit
+ git commit -a -m "Formatting the code before Metalama divorce."
+
+ # Enable code formatting for Metalama
 $env:MetalamaEmitCompilerTransformedFiles="true"
 $env:MetalamaFormatOutput="true"
+
+# Rebuild
 dotnet build /t:rebuild
+
+# Write generated code back to the source code
 metalama divorce
+
+## Format
+dotnet format
