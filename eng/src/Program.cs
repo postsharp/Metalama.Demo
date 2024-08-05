3eb4516b9517b7fed6b12d551e7fa7cf152e6a90
GaelFraiteur <gael@postsharp.net>
2024-08-05 11:54:38 +0200

    Updated C#.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -15,16 +15,24 @@ using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Defin
 
 var teamCityConfiguration = TeamCityHelper.CreateConfiguration( TeamCityHelper.GetProjectId( "Metalama.Demo" ) );
 
-var product = new Product(new DependencyDefinition( MetalamaDependencies.Family, "Metalama.Demo", "dev", "master", new GitHubRepository("Metalama.Demo" ), teamCityConfiguration, false))
-{
-    Solutions = new[] { new DotNetSolution("Metalama.Demo.sln") },
-    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering.ToDependency(), MetalamaDependencies.MetalamaPatterns },
-    Configurations = Product.DefaultConfigurations
-        .WithValue(BuildConfiguration.Public, Product.DefaultConfigurations.Public with { PublicPublishers = new Publisher[] { new MergePublisher() } })
-};
+var product =
+    new Product( new DependencyDefinition( MetalamaDependencies.Family, "Metalama.Demo", "dev", "master",
+        new GitHubRepository( "Metalama.Demo" ), teamCityConfiguration, false ) )
+    {
+        Solutions = [new DotNetSolution( "Metalama.Demo.sln" )],
+        Dependencies =
+        [
+            DevelopmentDependencies.PostSharpEngineering.ToDependency(), MetalamaDependencies.MetalamaPatterns
+        ],
+        Configurations = Product.DefaultConfigurations
+            .WithValue( BuildConfiguration.Public,
+                Product.DefaultConfigurations.Public with
+                {
+                    PublicPublishers = [new MergePublisher()]
+                } )
+    };
 
 var commandApp = new CommandApp();
-commandApp.AddProductCommands(product);
+commandApp.AddProductCommands( product );
 
-return commandApp.Run(args);
- 
+return commandApp.Run( args );
